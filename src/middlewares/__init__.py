a72c9c9fc5236afda87abc00840c0af050277de6
tabris17 <tabris17.cn@gmail.com>
2022-12-02 19:06:28 +0800

    更新 __init__.py

--- src/middlewares/__init__.py
+++ src/middlewares/__init__.py
@@ -7,7 +7,9 @@ from .cache import CacheMiddleware
 __all__ = ('Middleware', 'CacheMiddleware')
 
 
-def get_class(name):
+def create_middleware(name, next, kwargs):
     class_name = ''.join([_.capitalize() for _ in name.split('_')]) + 'Middleware'
     if class_name not in __all__: return
-    return getattr(sys.modules[__name__], class_name)
+    middleware_class = getattr(sys.modules[__name__], class_name)
+    return middleware_class(next, **kwargs) if kwargs else \
+        middleware_class(next)
