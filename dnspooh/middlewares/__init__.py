3d993ffa0dd5748e723bd6accf707eedad6e9e2d
tabris17 <tabris17.cn@gmail.com>
2022-12-15 19:04:59 +0800

    更新 __init__.py

--- dnspooh/middlewares/__init__.py
+++ dnspooh/middlewares/__init__.py
@@ -1,6 +1,5 @@
 import sys
 
-from .middleware import Middleware
 from .cache import CacheMiddleware
 from .hosts import HostsMiddleware
 
@@ -19,3 +18,37 @@ def create_middleware(name, next, config):
     elif isinstance(config, dict):
         return middleware_class(next, **config)
     return middleware_class(next, config)
+
+
+class Middleware:
+    @property
+    def server(self):
+        if hasattr(self, '_server'):
+            return self._server
+        from server import Server
+        if isinstance(self.next, Server):
+            self._server = self.next
+            return self._server
+        self._server =  self.next.server
+        return self._server
+
+    def __init__(self, next):
+        self.next = next
+
+    def abort(self):
+        return self.next.abort()
+
+    def abort(self):
+        return self.next.abort()
+
+    async def handle(self, request, *args, **kwarg):
+        return await self.next.handle(request, *args, **kwarg)
+
+    async def bootstrap(self):
+        return await self.next.bootstrap()
+
+    async def restart(self):
+        return await self.next.restart()
+
+    async def reload(self):
+        return await self.next.reload()
